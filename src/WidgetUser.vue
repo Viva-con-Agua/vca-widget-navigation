b710056e50b96c0e5e35d16a5cf92f7a8f90779d
Johann Sell <sell@informatik.hu-berlin.de>
2018-09-11 16:38:49 +0200

    first running version

--- src/WidgetUser.vue
+++ src/WidgetUser.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-if="defined">
-    <img v-if="showImg" :src="user.avatar" alt="Missing image" />
-    <span :class="typeData">{{ user.firstname }} {{this.user.lastname }}</span>
+  <div v-if="defined()">
+    <img v-if="showImg && user.profiles[0].avatar.length != 0" :src="user.profiles[0].avatar[0]" alt="Missing image" />
+    <span :class="typeData">{{ user.profiles[0].supporter.fullName }}</span>
   </div>
   <div v-else>
     <span :class="typeData">There is currently no user logged in!</span>
@@ -10,6 +10,9 @@
 
 <script>
   import Vue from 'vue'
+  import axios from 'axios'
+
+  // Vue.use(axios);
 
   export default {
     name: 'UserWidget',
@@ -22,32 +25,30 @@
         typeData: this.type
       }
     },
+    created () {
+      if (this.uuidData == null) {
+        this.uuidData = Vue.$widgetUserDefaultUUID
+      }
+      if (this.typeData == null) {
+        this.typeData = Vue.$widgetUserDefaultType
+      }
+      axios.get('/drops/widgets/user/' + this.uuidData)
+        .then(response => {
+          switch (response.status) {
+            case 200:
+              this.user = response.data.additional_information
+              break
+          }
+        }).catch(error => {
+        switch (error.response.status) {
+          case 404:
+            // Todo: How to handle error messages?
+            // that.open(that.$t('signin.error'), error.response.data.msg, "error");
+            break
+        }
+      })
+    },
     methods: {
-      init: function () {
-        if (this.uuidData == null) {
-          this.uuidData = Vue.$widgetUserDefaultUUID
-        }
-        if (this.typeData == null) {
-          this.typeData = Vue.$widgetUserDefaultType
-        }
-        var that = this
-        this.axios
-          .get('http://localhost/drops/widgets/user/' + this.uuidData)
-          .then(function (response) {
-            switch (response.status) {
-              case 200:
-                that.user = response.additional_information
-                break
-            }
-          }).catch(function (error) {
-            switch (error.response.status) {
-              case 404:
-              // Todo: How to handle error messages?
-              // that.open(that.$t('signin.error'), error.response.data.msg, "error");
-                break
-            }
-          })
-      },
       empty: function () {
         return this.user == null
       },
