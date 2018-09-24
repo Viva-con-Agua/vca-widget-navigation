fcbabd2c283aa6bf3a077c99d6cc5d4999f3a9fb
Johann Sell <sell@informatik.hu-berlin.de>
2018-09-24 15:32:37 +0200

    update details page

--- src/InfoField.vue
+++ src/InfoField.vue
@@ -9,9 +9,23 @@
     <span class="error">Forbidden</span>
   </div>
   <div v-else-if="defined()" class="user-infos" :class="type">
-    <span class="name">{{ user.profiles[0].supporter.fullName }}</span>
-    <span v-if="type !== 'small' && showCrew()" class="crew">({{ user.profiles[0].supporter.crew }})</span>
-    <span v-else class="crew">(No crew)</span>
+    <h2 class="name">{{ user.profiles[0].supporter.fullName }}</h2>
+    <div class="details">
+      <div class="personal details-column">
+        <span v-if="type !== 'small'" class="age">Alter: {{ getAge() }}</span>
+        <div v-if="type !== 'small'" class="contact">
+          <!--<span v-if="user.profiles[0].hasOwnProperty('email')" class="typcn typcn-mail"></span>-->
+          <!--<span v-if="user.profiles[0].supporter.hasOwnProperty('mobilePhone') && user.profiles[0].supporter.mobilePhone !== null" class="typcn typcn-phone"></span>-->
+          <div v-if="user.profiles[0].hasOwnProperty('email')" v-html="require('./images/mail.svg')" v-tooltip="user.profiles[0].email"/>
+          <div v-if="user.profiles[0].supporter.hasOwnProperty('mobilePhone') && user.profiles[0].supporter.mobilePhone !== null" v-html="require('./images/phone.svg')" v-tooltip="user.profiles[0].supporter.mobilePhone" />
+        </div>
+      </div>
+      <div class="vca details-column">
+        <span v-if="type !== 'small'" class="since">Dabei seit: {{ getSince() }}</span>
+        <span v-if="type !== 'small' && showCrew()" class="crew">{{ user.profiles[0].supporter.crew }}</span>
+        <span v-else-if="type !== 'small'" class="crew">Keine Crew</span>
+      </div>
+    </div>
   </div>
   <div v-else class="user-infos" :class="type">
     <span class="error">An unknown error occurred</span>
@@ -19,49 +33,68 @@
 </template>
 
 <script>
-    export default {
-      name: 'InfoField',
-      props: ['type', 'user', 'errorCode'],
-      methods: {
-        empty: function () {
-          return this.user == null || typeof this.user === 'undefined'
-        },
-        defined: function () {
-          return !this.empty()
-        },
-        showCrew: function () {
-          return this.defined() && this.user != null && typeof this.user !== 'undefined' &&
+  import Vue from 'vue'
+  import VTooltip from 'v-tooltip'
+
+  Vue.use(VTooltip)
+
+  export default {
+    name: 'InfoField',
+    props: ['type', 'user', 'errorCode'],
+    methods: {
+      empty: function () {
+        return this.user == null || typeof this.user === 'undefined'
+      },
+      defined: function () {
+        return !this.empty()
+      },
+      showCrew: function () {
+        return this.defined() && this.user != null && typeof this.user !== 'undefined' &&
               this.user.profiles[0].supporter.hasOwnProperty('crew')
-        },
-        error: function () {
-          return this.errorCode !== null
-        }
+      },
+      error: function () {
+        return this.errorCode !== null
+      },
+      getAge: function () {
+        var birthday = new Date(this.user.profiles[0].supporter.birthday)
+        var ageDifMs = Date.now() - birthday.getTime()
+        var ageDate = new Date(ageDifMs) // miliseconds from epoch
+        return Math.abs(ageDate.getUTCFullYear() - 1970)
+      },
+      getSince: function () {
+        var created = new Date(this.user.created)
+        return created.getUTCFullYear()
       }
     }
+  }
 </script>
 
 <style scoped>
-  .small span.name {
-    font-size: 0.8em;
+  h2.name {
+    margin: 0;
+  }
+
+  .small h2.name {
+    font-size: 0.7em;
     font-style: italic;
   }
 
   .small span.error {
-    font-size: 0.8em;
+    font-size: 0.7em;
     font-style: italic;
   }
 
-  .medium span.name {
-    font-size: 0.9em;
+  .medium h2.name {
+    font-size: 0.8em;
     font-weight: bold;
   }
 
   .medium span.error {
-    font-size: 0.9em;
+    font-size: 0.8em;
   }
 
-  .large span.name {
-    font-size: 1.2em;
+  .large h2.name {
+    font-size: 1em;
     font-weight: bolder;
   }
 
@@ -73,9 +106,46 @@
     color: grey;
   }
 
-  span.crew {
+  .details {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0.3em;
+  }
+
+  .details div {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .details div span {
     font-style: italic;
     font-size: 0.7em;
+  }
+
+  .details .personal {
+    text-align: left;
+  }
+
+  .details .personal .contact {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    height: 0.7em;
+  }
+
+  .details .personal .contact div {
+    height: 0.7em;
+  }
+
+  .details .personal .contact div >>> svg #icon {
+    fill: #0a6b91;
+  }
+
+  .details .vca {
+    text-align: right;
   }
 
   div.user-infos {
@@ -85,6 +155,7 @@
   }
 
   div.user-infos.large {
-    height: 3em;
+    margin-top: 0.3em;
+    height: 4em;
   }
 </style>
